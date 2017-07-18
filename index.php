04cf2603ffa2fd8547096721e6d5312e7cf4e7df
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-18 12:50:28 -0600

    changed to php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Shiny Apps
+		</title>
+		<?php include("header.html");?>
+			<main class="mdl-layout__content">
+				<div class="page-content">
+					<h1>
+						Shiny Apps
+					</h1>
+					<a href="http://grahamst.at">
+						Home
+					</a><br/>
+					<a href="http://grahamst.at/shiny/Simulation/">
+						Simulation
+					</a><br/>
+				</div>
+			</main>
+		</div>
+    </body>
+	<!-- Accent-colored raised button with ripple -->
+	<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+		Button
+	</button>
+	<!-- Colored FAB button -->
+	<button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+		<i class="material-icons">
+			add
+		</i>
+	</button>
+</html>
