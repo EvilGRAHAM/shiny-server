3a2beab9549af614d46db0c1dbe213dce5c9c1aa
EvilGRAHAM <scott.grah95@gmail.com>
2017-07-31 23:50:34 -0600

    Added baseball leaders

---
+++ Baseball_Leaders/top_n_leaderboard.R
@@ -0,0 +1,75 @@
+rm(list=ls())
+library(tidyverse)
+library(Lahman)
+library(lubridate)
+
+# Updates them_minimal so that there is borders around the graphs and the facet headings.
+theme_minimal2 <- theme_minimal() %>%  theme_set()
+theme_minimal2 <-
+  theme_update(
+    panel.border = element_rect(
+      linetype = "solid"
+      ,colour = "grey92"
+      ,fill = NA
+    )
+    ,strip.background = element_rect(
+      linetype = "solid"
+      ,colour = "grey92"
+      ,fill = NA
+    )
+  )
+
+# Year to go up to
+yr <- 1990
+n <- 10
+
+Batting_tib <- 
+  Batting %>% 
+  as.tibble()
+
+# Provides cumulative stats for each player up to this year.
+batting_cum <- 
+  Batting_tib %>% 
+  group_by(playerID) %>% 
+  mutate(Cum_HR = cumsum(HR)) %>% 
+  filter(yearID <= yr) %>% 
+  arrange(desc(Cum_HR)) %>% 
+  distinct(
+    playerID
+    ,.keep_all = TRUE
+  ) %>%
+  head(n) %>% 
+  mutate(
+    `First Name` = playerInfo(playerID)$nameFirst
+    ,`Last Name` = playerInfo(playerID)$nameLast
+  ) %>% 
+  mutate(
+    `Full Name` = paste0(`Last Name`, ", ", `First Name`)
+  )
+
+batting_cum %>% 
+  head(n) %>% 
+  mutate(
+    `First Name` = playerInfo(playerID)$nameFirst
+    ,`Last Name` = playerInfo(playerID)$nameLast
+  )
+
+
+top_n_bar_chart <- 
+  ggplot(
+    batting_cum%>% 
+      head(n)
+    ,aes(
+      x = `Full Name`
+      ,y = Cum_HR
+    )
+  ) +
+  geom_col(
+    alpha = 0.75
+  ) +
+  labs(
+    title = paste("Top", n, "Players by Cumulative HR's in", yr)
+    ,y = "Cumulative HR's"
+  )
+top_n_bar_chart
+  
