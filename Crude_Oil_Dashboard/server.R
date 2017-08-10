7bc891062a91ec12b9b3c31fc4b94ebeb236d938
EvilGRAHAM <scott.grah95@gmail.com>
2017-08-10 15:03:31 -0600

    Added table and charting

--- Crude_Oil_Dashboard/server.R
+++ Crude_Oil_Dashboard/server.R
@@ -3,8 +3,9 @@ library(shiny, warn.conflicts = FALSE, quietly = TRUE)
 # library(shinythemes, warn.conflicts = FALSE, quietly = TRUE)
 library(shinydashboard, warn.conflicts = FALSE, quietly = TRUE)
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
-library(tidyquant, warn.conflicts = FALSE, quietly = TRUE)
-# library(lubridate, warn.conflicts = FALSE, quietly = TRUE)
+library(readxl, warn.conflicts = FALSE, quietly = TRUE)
+# library(tidyquant, warn.conflicts = FALSE, quietly = TRUE)
+library(lubridate, warn.conflicts = FALSE, quietly = TRUE)
 library(DT, warn.conflicts = FALSE, quietly = TRUE)
 
 
@@ -26,14 +27,295 @@ theme_minimal2 <-
   )
 
 
-# Data Import ----------
-pricing_data <- 
-  read.csv("Data/pricing_data.csv")
+# Functions ----------
 
+summary_tab <- function(){}
+
+blended_summary_fun <- function(summary_data){
+  summary_data %>% 
+    group_by(Stream) %>% 
+    summarize(
+      `Overall Volume` = sum(`Spot Volume`)
+      ,`Overall Value` = sum(`Spot Volume` * `Spot Price`)
+      ,`Index Price` = round(sum(`Spot Volume` * `Spot Price`) / sum(`Spot Volume`), 4)
+      ,`Trades` = length(`Stream`)
+    )
+}
+
+stream_chart_fun <- function(chart_data, platform_input, stream_input){
+  chart_data %>% 
+    filter(
+      # Platform == platform_input
+      Stream == stream_input
+    ) %>%
+    group_by(
+      Stream
+      ,`Trade Date`
+      ,Platform
+    ) %>% 
+    select(
+      `Trade Date`
+      ,`Stream`
+      ,`Index Price`
+      ,`Spot Price`
+      ,Platform
+    ) %>% 
+    gather(
+      Method
+      ,Price
+      ,-`Trade Date`
+      ,-`Stream`
+      ,-Platform
+    ) %>%
+    ggplot(
+      aes(
+        x = `Trade Date`
+        ,y = Price
+        ,group = Method
+        ,colour = Method
+      )
+    ) +
+    geom_point() +
+    geom_smooth(se = FALSE) +
+    # facet_wrap(
+    #   ~ Stream
+    #   ,scales = "free"
+    # ) +
+    facet_grid(
+      Stream ~ Platform
+      ,scales = "free"
+    ) +
+    scale_colour_brewer(
+      type = "qual"
+      ,name = "Method:"
+      ,palette = "Set2"
+    )
+}
+
+
+# Data Import ----------
+data_path <- "Data/crude_oil_dashboard_data.xlsx"
+
+pricing_data <- 
+  read_excel(data_path, "pricing_data")
+
+nos_dates <- 
+  read_excel(data_path, "nos_dates")
+
+priority_streams <- 
+  read_excel(data_path, "priority_streams")
+
+pricing_data <- 
+  pricing_data %>% 
+  left_join(
+    nos_dates
+    ,by = "Trade Cycle"
+  ) %>% 
+  mutate(
+    priority = if_else(Stream %in% priority_streams$Stream, 1, 0)
+    ,in_trade_cycle = if_else(
+      `Trade Date` %within% interval(`Trade Cycle` - months(1), `NOS Date`)
+      ,1
+      ,0
+    )
+  ) %>% 
+  select(
+    -Notes
+  )
 
 # Server ----------
 shinyServer(
   function(input, output){
     
+    # Trade Data ----------
+    trade_data <- reactive({
+      pricing_data %>% 
+        filter(
+          `Trade Cycle` == as.Date(input$trade_cycle)
+          ,priority == 1
+          ,in_trade_cycle == 1
+        )
+    })
+    
+    # Summary Table ----------
+    output$summary_tbl <- renderDataTable({
+      
+      blended_summary <- blended_summary_fun(trade_data())
+      
+      summary_tbl <- 
+        trade_data() %>% 
+        group_by(
+          Stream
+          ,Platform
+        ) %>%
+        summarize(
+          `Trade ID` = max(`Trade ID`)
+          ,`Trades` = length(`Stream`)
+        ) %>% 
+        left_join(
+          trade_data() %>% 
+            ungroup() %>% 
+            select(-c(Stream, Platform))
+          ,by = "Trade ID"
+        ) %>% 
+        split(.$Platform)
+      
+      summary_tbl <-
+        full_join(
+          x = summary_tbl[[1]]
+          ,y = summary_tbl[[2]]
+          ,by = "Stream"
+          ,suffix = c(".NE", ".NGX")
+        ) %>% 
+        left_join(
+          y = blended_summary
+          ,by = "Stream"
+        ) %>%
+        ungroup() %>% 
+        select(
+          `Stream`
+          ,`Index Price`
+          ,`Overall Volume`
+          ,`Trades`
+          # ,starts_with("Trade ID.")
+          ,starts_with("Trade Date.")
+          ,starts_with("Spot Price.")
+          ,starts_with("Spot Volume.")
+          ,starts_with("Index Price.")
+          ,starts_with("Net Volume.")
+          ,starts_with("Trades.")
+        ) %>%
+        select(
+          `Stream`
+          ,`Index Price`
+          ,`Overall Volume`
+          ,`Trades`
+          ,ends_with(".NE")
+          ,ends_with(".NGX")
+        ) %>%
+        mutate(
+          `Trade Date.NE` = as.character(`Trade Date.NE`)
+          ,`Trade Date.NGX` = as.character(`Trade Date.NGX`)
+        ) %>% 
+        arrange(
+          Stream
+        )
+      
+    }
+    ,options = list(
+      pageLength = priority_streams %>% count() %>% as.numeric()
+    )
+    
+    )
+    
+    # Price Time Series Charts ----------
+    output$price_ts_charts <- renderPlot(stream_chart_fun(trade_data(), "NE", input$chart_stream))
+    
+    # Blended Index Price Chart ----------
+    output$blended_index_price_chart <-
+      renderPlot(
+        trade_data() %>% 
+          blended_summary_fun() %>%
+          ggplot(
+            aes(
+              x = `Stream`
+              ,y = `Index Price`
+            )
+          ) +
+          geom_col() +
+          theme(
+            axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)
+          )
+      )
+    
+    # Volume Split Chart ----------
+    output$volume_split_chart <-
+      renderPlot(
+        trade_data() %>% 
+          group_by(
+            Stream
+            ,Platform
+          ) %>% 
+          summarize(
+            `Overall Volume` = sum(`Spot Volume`)
+            ,`Overall Value` = sum(`Spot Volume` * `Spot Price`)
+            ,`Index Price` = round(sum(`Spot Volume` * `Spot Price`) / sum(`Spot Volume`), 4)
+            ,`Trades` = length(`Stream`)
+          ) %>% 
+          ggplot(
+            aes(
+              x = `Stream`
+              ,y = `Overall Volume`
+              ,colour = `Platform`
+              ,fill = `Platform`
+            )
+          ) +
+          geom_col(
+            alpha = 0.75
+            ,position = "fill"
+          ) +
+          geom_hline(
+            aes(
+              yintercept = 0.5
+            )
+            ,linetype = "dotted"
+          ) +
+          scale_colour_brewer(
+            type = "qual"
+            ,name = "Platform:"
+            ,palette = "Set2"
+          ) +
+          scale_fill_brewer(
+            type = "qual"
+            ,name = "Platform:"
+            ,palette = "Set2"
+          ) +
+          coord_flip()
+      )
+    
+    # Trade Split Chart ----------
+    output$trade_split_chart <-
+      renderPlot(
+        trade_data() %>% 
+          group_by(
+            Stream
+            ,Platform
+          ) %>% 
+          summarize(
+            `Overall Volume` = sum(`Spot Volume`)
+            ,`Overall Value` = sum(`Spot Volume` * `Spot Price`)
+            ,`Index Price` = round(sum(`Spot Volume` * `Spot Price`) / sum(`Spot Volume`), 4)
+            ,`Trades` = length(`Stream`)
+          ) %>% 
+          ggplot(
+            aes(
+              x = `Stream`
+              ,y = `Trades`
+              ,colour = `Platform`
+              ,fill = `Platform`
+            )
+          ) +
+          geom_col(
+            alpha = 0.75
+            ,position = "fill"
+          ) +
+          geom_hline(
+            aes(
+              yintercept = 0.5
+            )
+            ,linetype = "dotted"
+          ) +
+          scale_colour_brewer(
+            type = "qual"
+            ,name = "Platform:"
+            ,palette = "Set2"
+          ) +
+          scale_fill_brewer(
+            type = "qual"
+            ,name = "Platform:"
+            ,palette = "Set2"
+          ) +
+          coord_flip()
+      )
   }
 )
--- Crude_Oil_Dashboard/ui.R
+++ Crude_Oil_Dashboard/ui.R
@@ -3,6 +3,7 @@ library(shiny, warn.conflicts = FALSE, quietly = TRUE)
 # library(shinythemes, warn.conflicts = FALSE, quietly = TRUE)
 library(shinydashboard, warn.conflicts = FALSE, quietly = TRUE)
 library(tidyverse, warn.conflicts = FALSE, quietly = TRUE)
+library(readxl, warn.conflicts = FALSE, quietly = TRUE)
 # library(tidyquant, warn.conflicts = FALSE, quietly = TRUE)
 library(lubridate, warn.conflicts = FALSE, quietly = TRUE)
 library(DT, warn.conflicts = FALSE, quietly = TRUE)
@@ -13,11 +14,16 @@ db_header <- dashboardHeader(
   title = "Pricing Dashboard"
 )
 
-trade_cycle_list <- tibble(`Trade Cycle` = as.Date(NA))
+
+# Dashboard Sidebar ----------
+trade_cycle_list <- tibble(`Trade Cycle` = as_date(NA))
 for (i in -1:10){
   trade_cycle_list[i + 2, 1] <- floor_date(today(), unit = "months") - months(i)
 }
-# Dashboard Sidebar ----------
+data_path <- "Data/crude_oil_dashboard_data.xlsx"
+priority_streams <- 
+  read_excel(data_path, "priority_streams")
+
 db_sidebar <- dashboardSidebar(
   sidebarMenu(
     # Sidebar Menu List ----------
@@ -27,6 +33,12 @@ db_sidebar <- dashboardSidebar(
       inputId = "trade_cycle"
       ,label = "Enter the Trade Cycle:"
       ,choices = trade_cycle_list
+      ,selectize = TRUE
+    )
+    ,selectInput(
+      inputId = "base_stream"
+      ,label = "Enter the Base Stream:"
+      ,choices = priority_streams$Stream
       ,selectize = TRUE
     )
   )
@@ -39,6 +51,61 @@ db_body <- dashboardBody(
     
     tabItem(
       tabName = "db_pricing"
+      # Summary Table ----------
+      ,fluidRow(
+        
+        column(
+          12
+          ,box(
+            dataTableOutput("summary_tbl")
+            ,title = "Summary Table"
+            ,width = NULL
+            ,collapsible = TRUE
+          )
+        )
+      )
+      
+      # Charts ----------
+      ,fluidRow(
+        # Price Time Series Charts ----------
+        column(
+          8
+          ,box(
+            selectInput(
+              inputId = "chart_stream"
+              ,label = "Enter the Base Stream:"
+              ,choices = priority_streams$Stream
+              ,selectize = TRUE
+              ,multiple = TRUE
+            )
+            ,plotOutput("price_ts_charts")
+            ,title = "Time Series Charts"
+            ,width = NULL
+            ,collapsible = TRUE
+          )
+        )
+        # Summary Charts ----------
+        ,column(
+          4
+          ,tabBox(
+            title = "Summary Charts"
+            ,id = "summary_charts_tabset"
+            ,tabPanel(
+              "Blended Index Price Chart"
+              ,plotOutput("blended_index_price_chart")
+            )
+            ,tabPanel(
+              "Volume Split Chart"
+              ,plotOutput("volume_split_chart")
+            )
+            ,tabPanel(
+              "Trade Split Chart"
+              ,plotOutput("trade_split_chart")
+            )
+            ,width = NULL
+          )
+        )
+      )
     )
   )
 )
